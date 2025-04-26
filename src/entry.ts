3419fd32faff45e30e002f19ced0dad377e71594
Chargonium <svenvandoorn@icloud.com>
2025-04-26 09:42:06 +0200

    Added xprime nas, And some TMDB utils.

--- src/entry.ts
+++ src/entry.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response, NextFunction } from "express";
 import { createServer } from "http";
 import { URL } from "url";
 import { Readable } from "stream";
--- src/providers/sources/xprime.ts
+++ src/providers/sources/xprime.ts
@@ -1,5 +1,19 @@
 import { Request, Response } from "express";
 import { USER_AGENT } from "../../globals";
+import { getExternalIds } from "../../utils/tmdb";
+import firstTruthy from "../../utils/first_truthy";
+
+async function fetch_nas(id: string, User_Agent: string) {
+    let imdb_id = (await getExternalIds(Number(id))).imdb_id;
+    let resp = await fetch(`https://xprime.tv/nas?imdb=${imdb_id}`, {
+        headers: {
+            "User-Agent": User_Agent,
+        },
+    });
+    let json = await resp.json();
+    let quality = json.get("available_qualities")[0];
+    return json.get("streams").get(quality);
+}
 
 async function fetch_prime(id: string, User_Agent: string) {
     let resp = await fetch(`https://xprime.tv/primebox?id=${id}`, {
@@ -13,7 +27,18 @@ async function fetch_prime(id: string, User_Agent: string) {
 }
 
 async function scrape(id: string, req: Request) {
-    return fetch_prime(id, req.headers["user-agent"] || USER_AGENT);
+    let nas_fetcher = () =>
+        fetch_nas(id, req.headers["user-agent"] || USER_AGENT);
+    let prime_fetcher = () =>
+        fetch_prime(id, req.headers["user-agent"] || USER_AGENT);
+
+    const fastest = firstTruthy([nas_fetcher, prime_fetcher]);
+
+    try {
+        return await fastest;
+    } catch (error) {
+        return;
+    }
 }
 
 export = scrape;
